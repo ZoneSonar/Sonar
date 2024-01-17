f3b8da2e64ff137f4b01e046d9cd746bdd09832f
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2024-01-17 18:24:45 +0100

    formatting the code
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

---
+++ src/src/components/globals/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default 
+{
+  name: 'Header',
+  props: 
+  {
+  }
+}
+</script>
