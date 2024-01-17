7d5c525dbab68aa0a825d01b2de4a1c2c9dbc06a
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2024-01-17 18:02:35 +0100

    formatting the code
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

--- src/src/main.js
+++ src/src/main.js
@@ -1,14 +1,32 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { 
+    createApp 
+} from 'vue';
 
-import App from './App.vue'
-import router from './router'
+import { 
+    createPinia 
+} from 'pinia';
 
-const app = createApp(App)
+import App 
+    from './App.vue';
 
-app.use(createPinia())
-app.use(router)
+import router 
+    from './router';
 
-app.mount('#app')
+
+const app = createApp(
+    App
+);
+
+app.use(
+    createPinia()
+);
+
+app.use(
+    router
+);
+
+app.mount(
+    '#app'
+);
